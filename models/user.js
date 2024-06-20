af234d63e16d2643d9776e060ed458bd3f6ece6b
Naveen5676 <sample.first@yahoo.com>
2024-06-20 12:09:39 +0530

    Added user table

--- models/user.js
+++ models/user.js
@@ -1,16 +1,38 @@
-const Sequelize = require('sequelize');
+const { getDb } = require("../util/database");
+const mongodb = require("mongodb");
+const getdb = require("../util/database").getDb;
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+      .then((res) => {
+        console.log("inserted a user");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) })
+      .then((res) => {
+        console.log('find by id user',res);
+        return res
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
 module.exports = User;
